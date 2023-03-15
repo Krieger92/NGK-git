2dde48391c5fe93db7acc2d58225a35f534a4e2d
Gobz <ksprlrsn11@gmail.com>
2023-03-15 14:03:43 +0100

    Fjernet zeroing af data

--- Lektion4Server/server.c
+++ Lektion4Server/server.c
@@ -38,7 +38,7 @@ void sendFile(const int clientSocket, const char* fileName, long fileSize)
     numberOfBytes = fread(dataBuffer,1,sizeof(dataBuffer),fp);  // start læsning af fil
 
     while (numberOfBytes) {                                     // imens der stadig er mere at læse
-        bzero(dataBuffer,sizeof(dataBuffer));
+
         printf("Data: %li / %li\n",(fileSize-dataToSend), fileSize);   // opdater terminal
         
         write(clientSocket,dataBuffer,numberOfBytes);                   // skriv op til 1000 bytes til bruger
